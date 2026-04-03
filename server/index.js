3dea60f3681e2468d8e9b8c42b70a102bb326915
TiwariJi <gouravtiwari998@gmail.com>
2026-04-03 18:27:09 +0530

    updated server ports and cors

--- server/index.js
+++ server/index.js
@@ -1,20 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const app = express();
 const path = require('path');
 const roomRoutes = require('./routes/room');
 const codeRoutes = require('./routes/code');
 const Room = require('./models/Room');
-app.use(express.static(path.join(__dirname, '../client')));
-require('dotenv').config();
+const app = express();
 app.use(cors());
+app.use(express.static(path.join(__dirname, '../client')));
+
 app.use(express.json());
 app.use('/api/rooms', roomRoutes);
 app.use('/api/code',codeRoutes);
-app.get('/', (req, res) => {
-  res.send("Server is running")
-})
 const port = process.env.PORT || 5000
 
 mongoose.connect(process.env.MONGO_URI)
